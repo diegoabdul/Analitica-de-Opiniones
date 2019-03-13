b5fd7cb1155702f5c7a5cf1d40f959c59dece9c5
Diego Gil Edo <diego@MacBook-Pro-de-Diego-3.local>
2019-03-13 11:34:16 +0100

    Log In V 1.0
    admin; 123
    usuario; 123
    
    LOG IN añadido. Esos son los usuarios y contraseñas con los que se puede acceder al sistema de momento

---
+++ Controlador/ControladorVentanaLogin.py
@@ -0,0 +1,47 @@
+from Vista.VistaVentanaLogin import *
+import Controlador.ControladorVentanaPrincipal as ventanaPrincipal
+import Controlador.ControladorVentanaClasificador as ventanaClasificador
+
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self, *args, **kwargs):
+        QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
+        self.setupUi(self)
+        self.btn_entrar.clicked.connect(self.entrar)
+
+
+
+    def entrar(self):
+        """
+        Método encargado de ejecutar la ventana entrenar
+        """
+        print("hola")
+
+        usuario = self.textoUsuario.text()
+        contrasena = self.textoContrasena.text()
+
+        print(usuario)
+        print(contrasena)
+
+        adminCorrecto = "admin"
+        contrasenaAdminCorrecta = "123"
+
+        usuarioCorrecto = "usuario"
+        contrasenaUsuarioCorrecta = "123"
+
+
+        if (usuario == adminCorrecto) and (contrasena == contrasenaAdminCorrecta):
+            self.Open = ventanaPrincipal.MainWindow()
+            self.Open.show()
+            self.cerraVentana()
+
+        if (usuario == usuarioCorrecto) and (contrasena==contrasenaUsuarioCorrecta):
+            self.Open = ventanaClasificador.NewApp()
+            self.Open.show()
+            self.cerraVentana()
+
+
+    def cerraVentana(self):
+        """
+        Método encargado de cerrar la ventana actual
+        """
+        self.close()
--- Controlador/Launcher.py
+++ Controlador/Launcher.py
@@ -1,9 +1,13 @@
 from Vista.VistaVentanaPrincipal import *
+from Vista.VistaVentanaLogin import *
 import Controlador.ControladorVentanaPrincipal as ventanaPrincipal
+import Controlador.ControladorVentanaLogin as ventanaLogin
+
+
 
 def main():
     app = QtWidgets.QApplication([])
-    window = ventanaPrincipal.MainWindow()
+    window = ventanaLogin.MainWindow()
     window.show()
     app.exec_()
 
